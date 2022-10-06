2202e3e3d92d1dd8312e93c2b1ceb9b216eb1a0d
CohleM <manish981194@gmail.com>
2022-10-06 09:45:02 +0545

    refactoring code, no accuracy

--- snake.py
+++ snake.py
@@ -17,6 +17,9 @@ class Snake:
     
     def genFruit(self):
         self.fruit  =  [random.randint(0,9), random.randint(0,9)]
+        while ( ( self.fruit[0], self.fruit[1])) in self.body or ( ( self.fruit[0], self.fruit[1])) in self.head:
+            print('gg')
+            self.fruit  =  [random.randint(0,9), random.randint(0,9)]
     
     def ifEaten(self):
         self.body.insert(0, (self.head[0], self.head[1])) 
@@ -41,7 +44,7 @@ def play_snake(parameters):
     s.genFruit()
     s.body.append((s.head[0],s.head[1]-1))
     blue,red = (0, 0, 255) , (255, 0, 0) # hex code for blue
-    grid_size,snake_speed = 25,10000
+    grid_size,snake_speed = 25,5
     screen = pygame.display.set_mode((s.column*grid_size, s.row*grid_size))
     s.speedx,s.speedy = 0,-1
     game_over,running = False,True
@@ -54,18 +57,19 @@ def play_snake(parameters):
         Y = forward_propogation(X, parameters)
         dir_out = [(0,-1),(1,0),(0,1),(-1,0)]    #U R D L 
 
-        #Making sure it doesnot run in opposite direction 
-        if (-1*s.speedx,-1*s.speedy) == dir_out[np.argmax(Y)]:
-            b = Y.reshape(4)
-            c = (-b).argsort()[:2]
-            s.speedx, s.speedy = dir_out[c[1]]
-        else:
-            s.speedx, s.speedy = dir_out[np.argmax(Y)]
-
         #print([(s.fruit[0],s.fruit[1] )] , ' gg ')
         s.steps +=1
         screen.fill((0,0,0)) 
         
+        #Making sure it doesnot run in opposite direction 
+        if (-1*s.speedx,-1*s.speedy) == dir_out[np.argmax(Y)]:
+            game_over = True
+            s.fitness = (s.score + 0.5 + (0.5* ((s.score - (s.steps/(s.score + 1) ))/(s.score + (s.steps/ (s.score + 1) ) ) ) ))*1000000
+            return s.fitness,s.score,s.steps 
+        else:
+            s.speedx, s.speedy = dir_out[np.argmax(Y)]
+
+
         #When snake eats some fuit
         if( s.head == [s.fruit[0], s.fruit[1] ] ):
             print('True Eaten')
@@ -73,7 +77,7 @@ def play_snake(parameters):
             s.genFruit()
         
         #When it collides with itself or walls
-        if s.obstacles() or s.steps >= 200:
+        if s.obstacles() or s.steps >= 200 + 20*s.score:
             game_over = True
             s.fitness = (s.score + 0.5 + (0.5* ((s.score - (s.steps/(s.score + 1) ))/(s.score + (s.steps/ (s.score + 1) ) ) ) ))*1000000
             return s.fitness,s.score,s.steps
@@ -91,7 +95,6 @@ def play_snake(parameters):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 running = False
-
             if event.key == pygame.K_LEFT:
                 s.speedx = -1
                 s.speedy = 0
@@ -108,3 +111,4 @@ def play_snake(parameters):
         if event.type == pygame.QUIT:
           # Set running to False, stop event loop
           running = False
+

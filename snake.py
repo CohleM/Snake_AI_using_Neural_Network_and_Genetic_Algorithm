94db27286a4150cd44b36b13ea62240baaab8f27
CohleM <manish981194@gmail.com>
2023-03-29 08:41:46 +0545

    Enchanced AI

--- snake.py
+++ snake.py
@@ -44,7 +44,7 @@ def play_snake(parameters):
     s.genFruit()
     s.body.append((s.head[0],s.head[1]-1))
     blue,red = (0, 0, 255) , (255, 0, 0) # hex code for blue
-    grid_size,snake_speed = 25,10000
+    grid_size,snake_speed = 25,15
     screen = pygame.display.set_mode((s.column*grid_size, s.row*grid_size))
     s.speedx,s.speedy = 0,-1
     game_over,running = False,True
